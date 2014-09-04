2f70fd3df1d2236916e58cd8158fd70a3c683c65
Roy Six <roysix@users.noreply.github.com>
2014-09-04 13:12:49 -0400

    Update FontAwesome.java
    Removed final keywords, made Javadoc comments better conform to standards (removing obvious {@link} references), and removed one line method toUnicode since it was only being used in one place

--- FontAwesome.java
+++ FontAwesome.java
@@ -26,7 +26,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * <code>FontAwesome</code> converts Font Awesome icons to image files, such as in PNG or GIF format.
+ * {@code FontAwesome} converts Font Awesome icons to image files, such as in PNG or GIF format.
  * <p/>
  * For usage and examples, see the accompanying README.MD file.
  *
@@ -40,18 +40,18 @@ public class FontAwesome {
     /**
      * Main method.
      *
-     * @param args command line arguments
+     * @param args the command line arguments
      */
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         validateArgs(args);
         System.out.format( "\n%30s", "Initializing font... ");
-        final Font font = initFont();
+        Font font = initFont();
         System.out.print("Done!\n");
         System.out.format("%30s", "Initializing icons... ");
-        final Map<String, Character> icons = initIcons();
+        Map<String, Character> icons = initIcons();
         System.out.print("Done!\n");
         System.out.format("%30s", "Initializing properties... ");
-        final FontAwesome properties = initProperties(font, icons, args);
+        FontAwesome properties = initProperties(font, icons, args);
         System.out.print("Done!\n");
         System.out.format("%30s", "Saving " + icons.size() + " images... ");
         buildAndSaveImages(icons, properties);
@@ -61,9 +61,9 @@ public class FontAwesome {
     /**
      * Validates the command line arguments.
      *
-     * @param args command line arguments
+     * @param args the command line arguments
      */
-    public static void validateArgs(final String[] args) {
+    private static void validateArgs(String[] args) {
         if (args.length != 4 && args.length != 7) {
             System.out.print("\n\tFor regular icons, please enter 4 arguments:\n");
             System.out.print("\tjava FontAwesome [size] [color] [padding] [format]\n");
@@ -76,15 +76,15 @@ public class FontAwesome {
     }
 
     /**
-     * Initializes the Font Awesome {@link Font} via the provided TTF file.
+     * Initializes the Font Awesome font via the provided TTF file.
      *
-     * @return Font Awesome {@link Font}
+     * @return the font
      */
-    public static Font initFont() {
+    private static Font initFont() {
         Font font = null;
-        try (final FileInputStream fis = new FileInputStream("fonts/fontawesome-webfont.ttf")) {
+        try (FileInputStream fis = new FileInputStream("fonts/fontawesome-webfont.ttf")) {
             font = Font.createFont(Font.TRUETYPE_FONT, fis);
-        } catch (final IOException | FontFormatException e) {
+        } catch (IOException | FontFormatException e) {
             System.err.print(e.getMessage() + "\n");
             System.exit(1);
         }
@@ -92,23 +92,23 @@ public class FontAwesome {
     }
 
     /**
-     * Initializes the Font Awesome Icon {@link Map}, a mapping of {@link String} icon name keys to {@link Character}
-     * unicode values via the provided CSS file.
+     * Initializes the Font Awesome icon map, a mapping of icon name keys to unicode characters via the provided CSS 
+     * file.
      *
-     * @return Font Awesome Icon {@link Map}
+     * @return the icon map
      */
-    public static Map<String, Character> initIcons() {
-        final Map<String, Character> icons = new HashMap<>(1000);
+    private static Map<String, Character> initIcons() {
+        Map<String, Character> icons = new HashMap<>(1000);
         try {
-            final List<String> lines = Files.readAllLines(Paths.get("css/font-awesome.css"), StandardCharsets.UTF_8);
-            final Pattern pvalue = Pattern.compile("(?<=\\\\).*(?=\")"); // (?<=\).*(?=")
-            final Pattern pkey = Pattern.compile("(?<=.fa-).*(?=:before)"); // (?<=.fa-).*(?=:before)
+            List<String> lines = Files.readAllLines(Paths.get("css/font-awesome.css"), StandardCharsets.UTF_8);
+            Pattern pvalue = Pattern.compile("(?<=\\\\).*(?=\")"); // (?<=\).*(?=")
+            Pattern pkey = Pattern.compile("(?<=.fa-).*(?=:before)"); // (?<=.fa-).*(?=:before)
             for (int i = 0; i < lines.size(); i++) { // Check each line if it has a unicode value
-                final Matcher mvalue = pvalue.matcher(lines.get(i));
+                Matcher mvalue = pvalue.matcher(lines.get(i));
                 if (mvalue.find()) {
-                    final Character value = toUnicode(mvalue.group());
+                    Character value = (char) Integer.parseInt(mvalue.group(), 16);
                     for (int j = i - 1; j >= 0; j--) { // Check previous lines for the keys to this unicode value
-                        final Matcher mkey = pkey.matcher(lines.get(j));
+                        Matcher mkey = pkey.matcher(lines.get(j));
                         if (mkey.find()) {
                             icons.put(mkey.group(), value);
                         } else {
@@ -117,7 +117,7 @@ public class FontAwesome {
                     }
                 }
             }
-        } catch (final IOException e) {
+        } catch (IOException e) {
             System.err.print(e.getMessage() + "\n");
             System.exit(1);
         }
@@ -125,15 +125,15 @@ public class FontAwesome {
     }
 
     /**
-     * Initializes the {@link FontAwesome} properties via the provided program arguments.
+     * Initializes the FontAwesome properties via the provided program arguments.
      *
-     * @param font  Font Awesome {@link Font}
-     * @param icons Font Awesome Icon {@link Map}
-     * @param args  command line arguments
-     * @return      {@link FontAwesome} properties
+     * @param font  the font
+     * @param icons the icon map
+     * @param args  the command line arguments
+     * @return      the FontAwesome properties
      */
-    public static FontAwesome initProperties(final Font font, final Map<String, Character> icons, final String[] args) {
-        final FontAwesome properties = new FontAwesome();
+    private static FontAwesome initProperties(Font font, Map<String, Character> icons, String[] args) {
+        FontAwesome properties = new FontAwesome();
         properties.fsize = args.length > 0 ? Integer.parseInt(args[0]) : 48;
         properties.fcolor = args.length > 1 ? new Color(Integer.parseInt(args[1], 16)) : new Color(0);
         properties.padding = args.length > 2 && args[2].contains("/") ? Float.parseFloat(args[2].split("/")[0]) / Float.parseFloat(args[2].split("/")[1]) : 0;
@@ -148,99 +148,88 @@ public class FontAwesome {
     }
 
     /**
-     * Builds and saves all images in the icons map based on the {@link FontAwesome} properties in the images folder.
+     * Builds and saves all images in the icons map based on the FontAwesome properties in the images folder.
      *
-     * @param icons      Font Awesome Icon {@link Map}
-     * @param properties {@link FontAwesome} properties
+     * @param icons      the icon map
+     * @param properties the FontAwesome properties
      */
-    public static void buildAndSaveImages(final Map<String, Character> icons, final FontAwesome properties) {
-        final File images = new File("images");
+    private static void buildAndSaveImages(Map<String, Character> icons, FontAwesome properties) {
+        File images = new File("images");
         if (!images.exists()) { // Create the images folder if it doesn't already exit
             images.mkdir();
         }
-        for (final Map.Entry<String, Character> entry : icons.entrySet()) {
+        for (Map.Entry<String, Character> entry : icons.entrySet()) {
             saveImage(buildImage(properties.size, properties.sfont, properties.sicon, properties.scolor, properties.ffont,
                                  entry.getValue().toString(), properties.fcolor), "images/" + entry.getKey(), properties.format);
         }
     }
 
     /**
-     * Builds an {@link BufferedImage} with the drawn icon graphic based on the {@link Font}, size, and color.
+     * Builds a BufferedImage with the drawn icon graphic based on the font, size, and color.
      *
-     * @param size   encapsulating image size
-     * @param sfont  stacked {@link Font}
-     * @param sicon  stacked icon unicode value
-     * @param scolor stacked {@link Color}
-     * @param ffont  front {@link Font}
-     * @param ficon  front icon unicode value
-     * @param fcolor front {@link Color}
-     * @return       {@link BufferedImage} containing the drawn icon graphic
+     * @param size   the encapsulating image size
+     * @param sfont  the stacked font
+     * @param sicon  the stacked icon unicode value
+     * @param scolor the stacked color
+     * @param ffont  the front font
+     * @param ficon  the front icon unicode value
+     * @param fcolor the front color
+     * @return       the BufferedImage containing the drawn icon graphic
      */
-    public static BufferedImage buildImage(final int size,
-                                           final Font sfont, final String sicon, final Color scolor,
-                                           final Font ffont, final String ficon, final Color fcolor) {
-        final BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
-        final Graphics2D graphics = image.createGraphics();
+    private static BufferedImage buildImage(int size,
+                                           Font sfont, String sicon, Color scolor,
+                                           Font ffont, String ficon, Color fcolor) {
+        BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics = image.createGraphics();
         graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON); // AA
         if (sicon != null) { // Then draw stacked icon first
             graphics.setFont(sfont);
             graphics.setColor(scolor);
-            final Point spoint = calcDrawPoint(sfont, sicon, size, graphics);
+            Point spoint = calcDrawPoint(sfont, sicon, size, graphics);
             graphics.drawString(sicon, spoint.x, spoint.y);
         }
         graphics.setFont(ffont);
         graphics.setColor(fcolor);
-        final Point fpoint = calcDrawPoint(ffont, ficon, size, graphics);
+        Point fpoint = calcDrawPoint(ffont, ficon, size, graphics);
         graphics.drawString(ficon, fpoint.x, fpoint.y);
         graphics.dispose();
         return image;
     }
 
     /**
-     * Saves the generated {@link BufferedImage} to the hard drive in the images folder.
+     * Saves the generated images to the hard drive in the images folder.
      *
-     * @param image      {@link BufferedImage} containing the drawn icon graphic
-     * @param fileName   file name including path, but not including the file extension e.g. "square" or "images/square"
-     * @param formatName format name (file extension) to use e.g. "png" or "gif"
+     * @param image      the BufferedImage containing the drawn icon graphic
+     * @param fileName   the file name including path, but not including the file extension e.g. "square" or "images/square"
+     * @param formatName the format name (file extension) to use e.g. "png" or "gif"
      */
-    public static void saveImage(final BufferedImage image, final String fileName, final String formatName) {
+    private static void saveImage(BufferedImage image, String fileName, String formatName) {
         try {
             ImageIO.write(image, formatName, new File(fileName + "." + formatName));
-        } catch (final IOException e) {
+        } catch (IOException e) {
             System.err.print(e.getMessage() + "\n");
             System.exit(1);
         }
     }
 
     /**
-     * Converts a unicode {@link String} to a properly formatted unicode {@link Character}.
-     *
-     * @param str {@link String} input
-     * @return    {@link Character} in unicode format
-     */
-    public static Character toUnicode(final String str) {
-        return (char) Integer.parseInt(str, 16);
-    }
-
-    /**
-     * Calculates the {@link Point} to draw this icon with this {@link Graphics2D}. This will be the baseline lower-left
-     * point (X,Y) coordinates and will be centered. The calculation is done using the FontMetrics and GlyphVector
-     * rectangle bounds.
+     * Calculates the point to draw this icon with this Graphics2D. This will be the baseline lower-left point (X,Y)
+     * coordinates and will be centered. The calculation is done using the FontMetrics and GlyphVector rectangle bounds.
      * <p/>
      * The centering technique used is based off of Exploding Pixels's "Drawing Text About Its Visual Center."
      *
-     * @param font     {@link Font} to use (differs only in size)
-     * @param icon     icon unicode value to draw
-     * @param size     encapsulating image size of the bounding box
-     * @param graphics {@link Graphics2D} from the {@link BufferedImage}
-     * @return         {@link Point} containing the x and y coordinates needed to draw the icon centered
+     * @param font     the font to use (differs only in size)
+     * @param icon     the icon unicode value to draw
+     * @param size     the encapsulating image size of the bounding box
+     * @param graphics the Graphics2D from the BufferedImage
+     * @return         the point containing the x and y coordinates needed to draw the icon centered
      * @see            <a href="http://explodingpixels.wordpress.com/2009/01/29/drawing-text-about-its-visual-center/">
      *                 Exploding Pixels Drawing Text About Its Visual Center</a>
      */
-    public static Point calcDrawPoint(final Font font, final String icon, final int size, final Graphics2D graphics) {
-        final int center = size / 2; // Center X and Center Y are the same
-        final Rectangle stringBounds = graphics.getFontMetrics().getStringBounds(icon, graphics).getBounds();
-        final Rectangle visualBounds = font.createGlyphVector(graphics.getFontRenderContext(), icon).getVisualBounds().getBounds();
+    private static Point calcDrawPoint(Font font, String icon, int size, Graphics2D graphics) {
+        int center = size / 2; // Center X and Center Y are the same
+        Rectangle stringBounds = graphics.getFontMetrics().getStringBounds(icon, graphics).getBounds();
+        Rectangle visualBounds = font.createGlyphVector(graphics.getFontRenderContext(), icon).getVisualBounds().getBounds();
         return new Point(center - stringBounds.width / 2, center - visualBounds.height / 2 - visualBounds.y);
     }
 
