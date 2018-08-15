9b35ec4adbfd62031469cf37bd5aea8b2fbfdf63
roysix <royxsix@gmail.com>
2018-08-15 13:29:43 -0400

    .

--- FontAwesome.java
+++ FontAwesome.java
@@ -1,10 +1,11 @@
 /*
  * Copyright (c) 2014 Roy Six
- * Font Awesome by Dave Gandy - http://fontawesome.io
+ * Font Awesome by Dave Gandy - https://fontawesome.com
  */
 
-import javax.imageio.ImageIO;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
@@ -16,17 +17,20 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import javax.imageio.ImageIO;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 /**
- * {@code FontAwesome} converts Font Awesome icons to image files, such as in PNG or GIF format.
+ * {@code FontAwesome} converts Font Awesome icons to PNG image files.
  * <p/>
  * For usage and examples, see the accompanying README.MD file.
  *
@@ -35,7 +39,32 @@ import java.util.regex.Pattern;
  */
 public class FontAwesome {
 
-    int size; float padding; Font ffont; float fsize; Color fcolor; Font sfont; float ssize; Color scolor; String sicon; String format;
+    private static final String ICONS_JSON_PATH = "metadata/icons.json";
+    private static final String BRANDS_FONT_PATH = "otfs/Font Awesome 5 Brands-Regular-400.otf";
+    private static final String REGULAR_FONT_PATH = "otfs/Font Awesome 5 Free-Regular-400.otf";
+    private static final String SOLID_FONT_PATH = "otfs/Font Awesome 5 Free-Solid-900.otf";
+
+    private static class Properties {
+        List<String> icons;
+        List<String> styles;
+        int size;
+        float padding;
+        //    private static Font ffont;
+        float fsize;
+        Color fcolor; // Front variables
+        Font sfont;
+        float ssize;
+        Color scolor;
+        String sicon;  // Stacked variables
+        boolean transparent;
+        Color bgcolor;
+    }
+
+    private static class Icon {
+        String name;
+        List<String> styles;
+        Character unicode;
+    }
 
     /**
      * Main method.
@@ -44,17 +73,21 @@ public class FontAwesome {
      */
     public static void main(String[] args) {
         validateArgs(args);
-        System.out.format( "\n%30s", "Initializing font... ");
-        Font font = initFont();
+
+        System.out.format("\n%30s", "Initializing properties... ");
+        Properties properties = initProperties(args);
         System.out.print("Done!\n");
+
+        System.out.format( "%30s", "Initializing fonts... ");
+        Map<String, Font> fonts = initFonts(properties);
+        System.out.print("Done!\n");
+
         System.out.format("%30s", "Initializing icons... ");
-        Map<String, Character> icons = initIcons();
+        List<Icon> icons = initIcons(properties);
         System.out.print("Done!\n");
-        System.out.format("%30s", "Initializing properties... ");
-        FontAwesome properties = initProperties(font, icons, args);
-        System.out.print("Done!\n");
+
         System.out.format("%30s", "Saving " + icons.size() + " images... ");
-        buildAndSaveImages(icons, properties);
+        buildAndSaveImages(properties, fonts, icons);
         System.out.print("Done!\n");
     }
 
@@ -64,60 +97,101 @@ public class FontAwesome {
      * @param args the command line arguments
      */
     private static void validateArgs(String[] args) {
-        if (args.length != 4 && args.length != 7) {
-            System.out.print("\n\tFor regular icons, please enter 4 arguments:\n");
-            System.out.print("\tjava FontAwesome [size] [color] [padding] [format]\n");
-            System.out.print("\tex: \"java FontAwesome 48 0 1/8 png\"\n\n");
-            System.out.print("\tFor stacked icons, please enter 7 arguments:\n");
-            System.out.print("\tjava FontAwesome [size] [color] [padding] [format] [sicon] [ssize] [scolor]\n");
-            System.out.print("\tex: \"java FontAwesome 24 ffffff 0 png square 48 0\"\n");
+        if (args.length != 5 && args.length != 6 && args.length != 8) {
+            System.out.print("\n\tFor regular icons, enter 5 arguments:\n");
+            System.out.print("\tjava FontAwesome [icons] [styles] [size] [color] [padding]\n");
+            System.out.print("\tex: \"java FontAwesome all all 128 ff0000 1/8\"\n\n");
+            System.out.print("\tFor transparent icons, enter 6 arguments:\n");
+            System.out.print("\tjava FontAwesome [icons] [styles] [size] transparent [padding] [bgcolor]\n");
+            System.out.print("\tex: \"java FontAwesome plus-circle,minus-circle 128 transparent 1/8 ffffff\"\n\n");
+            System.out.print("\tFor stacked icons, enter 8 arguments:\n");
+            System.out.print("\tjava FontAwesome [icons] [styles] [size] [color] [padding] [sicon] [ssize] [scolor]\n");
+            System.out.print("\tex: \"java FontAwesome all 128 ffffff 0 png square 256 000000\"\n\n");
+            System.out.print("\tNote: [icons] and [styles] can be \"all\" or the individual icon/style names separated by commas e.g. \"plus-circle,minus-circle\" or \"regular,solid\"\n");
             System.exit(0);
         }
     }
 
     /**
-     * Initializes the Font Awesome font via the provided TTF file.
+     * Initializes the FontAwesome properties via the provided program arguments.
      *
-     * @return the font
+     * @param args  the command line arguments
      */
-    private static Font initFont() {
-        Font font = null;
-        try (FileInputStream fis = new FileInputStream("fonts/fontawesome-webfont.ttf")) {
-            font = Font.createFont(Font.TRUETYPE_FONT, fis);
-        } catch (IOException | FontFormatException e) {
-            System.err.print(e.getMessage() + "\n");
-            System.exit(1);
-        }
-        return font;
+    private static Properties initProperties(String[] args) {
+        Properties properties = new Properties();
+        properties.icons = Arrays.asList(args[0].split(","));
+        properties.styles = Arrays.asList(args[1].split(","));
+//        properties.ficons = args.length > 0 ? Arrays.asList(args[0].split(",")) : null;
+        properties.fsize = args.length > 2 ? Integer.parseInt(args[2]) : 48;
+        properties.fcolor = args.length > 3 ? "transparent".equals(args[3]) ? new Color(0x0000000, true) : new Color(Integer.parseInt(args[3], 16)) : new Color(0);
+        properties.transparent = args.length > 3 && "transparent".equals(args[3]);
+        properties.padding = args.length > 4 && args[4].contains("/") ? Float.parseFloat(args[4].split("/")[0]) / Float.parseFloat(args[4].split("/")[1]) : 0;
+        // properties.padding = args.length > 4 ? Float.parseFloat(args[4]) : 0;
+       properties.bgcolor = args.length > 5 && properties.transparent ? new Color(Integer.parseInt(args[5], 16)) : null;
+        //sicon = args.length > 5 && !transparent ? icons.get(args[5]).toString() : null;
+        properties.sicon = null;
+        properties.ssize = args.length > 6 ? Integer.parseInt(args[6]) : 0;
+        properties.scolor = args.length > 7 ? new Color(Integer.parseInt(args[7], 16)) : null;
+        properties.size = properties.fsize > properties.ssize ? (int) properties.fsize : (int) properties.ssize;
+//        properties.ffont = font.deriveFont(properties.fsize - properties.fsize * properties.padding);
+//        properties.sfont = properties.sicon != null ? font.deriveFont(properties.ssize - properties.ssize * properties.padding) : null;
+        return properties;
     }
 
     /**
-     * Initializes the Font Awesome icon map, a mapping of icon name keys to unicode characters via the provided CSS 
-     * file.
+     * Initializes the Font Awesome fonts via the provided TTF/OTF files and properties size and padding.
      *
-     * @return the icon map
+     * @param properties the properties containing the size and padding
+     * @return the fonts
      */
-    private static Map<String, Character> initIcons() {
-        Map<String, Character> icons = new HashMap<>(1000);
+    private static Map<String, Font> initFonts(Properties properties) {
+        Map<String, Font> fonts = new HashMap<>();
+        Map<String, String> paths = new HashMap<>();
+        paths.put("brands", BRANDS_FONT_PATH);
+        paths.put("regular", REGULAR_FONT_PATH);
+        paths.put("solid", SOLID_FONT_PATH);
+        for (Map.Entry<String, String> path : paths.entrySet()) {
+            try (FileInputStream fis = new FileInputStream(path.getValue())) {
+                Font font = Font.createFont(Font.TRUETYPE_FONT, fis).deriveFont(properties.size + (properties.padding * properties.size));
+                fonts.put(path.getKey(), font);
+            } catch (IOException | FontFormatException e) {
+                System.err.print(e.getMessage() + "\n");
+                System.exit(1);
+            }
+        }
+        return fonts;
+    }
+
+    /**
+     * TODO
+     *
+     * @param properties
+     * @return
+     */
+    private static List<Icon> initIcons(Properties properties) {
+        List<Icon> icons = new ArrayList<>();
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("javascript");
         try {
-            List<String> lines = Files.readAllLines(Paths.get("css/font-awesome.css"), StandardCharsets.UTF_8);
-            Pattern pvalue = Pattern.compile("(?<=\\\\).*(?=\")"); // (?<=\).*(?=")
-            Pattern pkey = Pattern.compile("(?<=.fa-).*(?=:before)"); // (?<=.fa-).*(?=:before)
-            for (int i = 0; i < lines.size(); i++) { // Check each line if it has a unicode value
-                Matcher mvalue = pvalue.matcher(lines.get(i));
-                if (mvalue.find()) {
-                    Character value = (char) Integer.parseInt(mvalue.group(), 16);
-                    for (int j = i - 1; j >= 0; j--) { // Check previous lines for the keys to this unicode value
-                        Matcher mkey = pkey.matcher(lines.get(j));
-                        if (mkey.find()) {
-                            icons.put(mkey.group(), value);
-                        } else {
-                            break;
+            byte[] encoded = Files.readAllBytes(Paths.get(ICONS_JSON_PATH));
+            String json = new String(encoded);
+            String script = "Java.asJSONCompatible(" + json + ")";
+            ScriptObjectMirror mirror = (ScriptObjectMirror) engine.eval(script);
+            for (Map.Entry<String, Object> entry : mirror.entrySet()) {
+                if (properties.icons.contains("all") || properties.icons.contains(entry.getKey())) {
+                    Icon icon = new Icon();
+                    icon.name = entry.getKey();
+                    ScriptObjectMirror mirror2 = (ScriptObjectMirror) entry.getValue();
+                    for (Map.Entry<String, Object> entry2 : mirror2.entrySet()) {
+                        if ("styles".equals(entry2.getKey())) {
+                            icon.styles = Arrays.asList(entry2.getValue().toString().replace("[", "").replace("]", "").split(", "));
+                        } else if ("unicode".equals(entry2.getKey())) {
+                            icon.unicode = (char) Integer.parseInt(entry2.getValue().toString(), 16);
                         }
                     }
+                    icons.add(icon);
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException | ScriptException e) {
             System.err.print(e.getMessage() + "\n");
             System.exit(1);
         }
@@ -125,42 +199,26 @@ public class FontAwesome {
     }
 
     /**
-     * Initializes the FontAwesome properties via the provided program arguments.
-     *
-     * @param font  the font
-     * @param icons the icon map
-     * @param args  the command line arguments
-     * @return      the FontAwesome properties
-     */
-    private static FontAwesome initProperties(Font font, Map<String, Character> icons, String[] args) {
-        FontAwesome properties = new FontAwesome();
-        properties.fsize = args.length > 0 ? Integer.parseInt(args[0]) : 48;
-        properties.fcolor = args.length > 1 ? new Color(Integer.parseInt(args[1], 16)) : new Color(0);
-        properties.padding = args.length > 2 && args[2].contains("/") ? Float.parseFloat(args[2].split("/")[0]) / Float.parseFloat(args[2].split("/")[1]) : 0;
-        properties.format = args.length > 3 ? args[3] : "png";
-        properties.sicon = args.length > 4 ? icons.get(args[4]).toString() : null;
-        properties.ssize = args.length > 5 ? Integer.parseInt(args[5]) : 0;
-        properties.scolor = args.length > 6 ? new Color(Integer.parseInt(args[6], 16)) : null;
-        properties.size = properties.fsize > properties.ssize ? (int) properties.fsize : (int) properties.ssize;
-        properties.ffont = font.deriveFont(properties.fsize - properties.fsize * properties.padding);
-        properties.sfont = properties.sicon != null ? font.deriveFont(properties.ssize - properties.ssize * properties.padding) : null;
-        return properties;
-    }
-
-    /**
      * Builds and saves all images in the icons map based on the FontAwesome properties in the images folder.
      *
      * @param icons      the icon map
-     * @param properties the FontAwesome properties
      */
-    private static void buildAndSaveImages(Map<String, Character> icons, FontAwesome properties) {
+    private static void buildAndSaveImages(Properties properties, Map<String, Font> fonts, List<Icon> icons) {
         File images = new File("images");
         if (!images.exists()) { // Create the images folder if it doesn't already exit
             images.mkdir();
         }
-        for (Map.Entry<String, Character> entry : icons.entrySet()) {
-            saveImage(buildImage(properties.size, properties.sfont, properties.sicon, properties.scolor, properties.ffont,
-                                 entry.getValue().toString(), properties.fcolor), "images/" + entry.getKey(), properties.format);
+        for (final String style : fonts.keySet()) {
+            File subimages = new File("images/" + style);
+            if (!subimages.exists()) {
+                subimages.mkdir();
+            }
+        }
+        for (Icon icon : icons) {
+            for (String style : icon.styles) {
+                BufferedImage image = buildImage(properties.size, properties.sfont, properties.sicon, properties.scolor, fonts.get(style), icon.unicode.toString(), properties.fcolor, properties.transparent, properties.bgcolor);
+                saveImage(image, "images/" + style + "/" + icon.name, "png");
+            }
         }
     }
 
@@ -174,15 +232,24 @@ public class FontAwesome {
      * @param ffont  the front font
      * @param ficon  the front icon unicode value
      * @param fcolor the front color
+     * @param transparent boolean indicating if transparent
+     * @param bgcolor     for transparency, the background color
      * @return       the BufferedImage containing the drawn icon graphic
      */
     private static BufferedImage buildImage(int size,
                                            Font sfont, String sicon, Color scolor,
-                                           Font ffont, String ficon, Color fcolor) {
+                                           Font ffont, String ficon, Color fcolor,
+                                           boolean transparent, Color bgcolor) {
         BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
         Graphics2D graphics = image.createGraphics();
         graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON); // AA
-        if (sicon != null) { // Then draw stacked icon first
+        // If transparent, set background color for transparent icons
+        if (transparent) {
+            graphics.setColor(bgcolor);
+            graphics.fillRect(0, 0, size, size);
+        }
+        // If stacked icon, then draw stacked icon first
+        if (sicon != null) {
             graphics.setFont(sfont);
             graphics.setColor(scolor);
             Point spoint = calcDrawPoint(sfont, sicon, size, graphics);
@@ -190,6 +257,9 @@ public class FontAwesome {
         }
         graphics.setFont(ffont);
         graphics.setColor(fcolor);
+        if (transparent) {
+            graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_IN, 1f));
+        }
         Point fpoint = calcDrawPoint(ffont, ficon, size, graphics);
         graphics.drawString(ficon, fpoint.x, fpoint.y);
         graphics.dispose();
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-font-awesome-to-image
-=====================
+# Font Awesome To Image
 
-Generates Font Awesome icon images. Tested to work with the latest Font Awesome release, Version 4.6.3 to generate 730 images (including aliases).
+Generates Font Awesome icon images. Tested to work with Font Awesome 5, Version 5.2.0 to generate 1K+ images (including aliases).
 
 <img src="https://raw.githubusercontent.com/roysix/font-awesome-to-image/master/screenshots/program-intro.png" alt="Program Intro" width="75%"/>
 
@@ -10,33 +9,45 @@ This is a [Java](http://www.oracle.com/technetwork/java/javase/downloads/index.h
 ### Set Up
 <img src="https://raw.githubusercontent.com/roysix/font-awesome-to-image/master/screenshots/setup.png" alt="Set Up" width="50%"/>
 
-1. Download [Font Awesome](http://fontawesome.io) and unzip it
-2. Place [`FontAwesome.java`](https://raw.githubusercontent.com/roysix/font-awesome-to-image/master/FontAwesome.java) in the `font-awesome-x.x.x` folder
+1. Download [Font Awesome Free for the Desktop](https://fontawesome.com/how-to-use/on-the-desktop/setup/getting-started) and unzip it
+2. Place [`FontAwesome.java`](https://raw.githubusercontent.com/roysix/font-awesome-to-image/master/FontAwesome.java) in the `font-awesome-free-x.x.x-desktop` folder
 3. Open up a command prompt or terminal shell and run the command `javac FontAwesome.java` to compile
+Note: For Font Awesome Version 4.7 or Black Tie, please use `FontAwesome4.7/FontAwesome.java` or `BlackTie/BlackTie.java`
 
 ### How To Use
+
+You can create regular icons, transparent icons, or stacked icons.
 
 ##### Regular Icons
 <img src="https://raw.githubusercontent.com/roysix/font-awesome-to-image/master/screenshots/regular-icons.png" alt="Regular Icons" width="50%"/>
 
-* Run `java FontAwesome [size] [color] [padding] [format]`
-* Example `java FontAwesome 48 0 1/8 png`
-* *Generates 48px black png icon images with a padding of 1/8 the size (48 * 1/8 = 6px)*
+* Run `java FontAwesome [styles] [icons] [size] [color] [padding]`
+* Example `java FontAwesome all all 128 FF0000 1/8`
+* *Generates 128px red (FF0000) png icon images with a padding of 1/8 the size (48 * 1/8 = 6px)*
 
-##### Stacked Icons
+##### Transparent Icons
+<img src="https://raw.githubusercontent.com/roysix/font-awesome-to-image/master/screenshots/regular-icons.png" alt="Regular Icons" width="50%"/>
+
+* Run `java FontAwesome [styles] [icons] [size] transparent [padding] [bgcolor]`
+* Example `java FontAwesome all all 128 transparent 1/8 FFFFFF`
+* *Generates 128px transparent png icon images with a background color of white (FFFFFF) and a padding of 1/8 the size (48 * 1/8 = 6px)*
+
+##### Stacked Icons (In Progress for Version 5...)
 <img src="https://raw.githubusercontent.com/roysix/font-awesome-to-image/master/screenshots/stacked-icons.png" alt="Stacked Icons" width="50%"/>
 
 * Run `java FontAwesome [size] [color] [padding] [format] [sicon] [ssize] [scolor]`
 * Example `java FontAwesome 24 ffffff 0 png square 48 0`
 * *Generates images that have a 24px white icon stacked on top of a 48px black square with no padding*
 
-> #### Tips!
-> *Use bigger image sizes for better quality when stacking icons
-<br/>
-Enter colors in [hex triplet](http://en.wikipedia.org/wiki/Web_colors#Hex_triplet) format like `ff69b4` (pink) or `0` (black)
-<br/>
-Enter padding as a fraction of the image size like `1/16` or `0` for none (`1/8` recommended)
-<br/>
-Add more padding if a particularly wide or tall icon is not able to fit in the desired image size
-<br/>
-Images will be saved in a newly made `images` folder*
+#### Legend
+- [styles] - Can be `all`, `brands`, `regular`, `solid`, `light`, or any combination of the four delimited by commas e.g. `brands,regular`
+- [icons] - Can be `all` or a list of icon names delimited by commas e.g. `plus-circle,minus-circle`
+- [color] - Can be a [hex triplet](http://en.wikipedia.org/wiki/Web_colors#Hex_triplet) format like `FF69B4` (pink) or `0` (black) or `transparent`
+
+
+#### Tips!
+> Use bigger image sizes for better quality when stacking icons
+> Enter colors in [hex triplet](http://en.wikipedia.org/wiki/Web_colors#Hex_triplet) format like `ff69b4` (pink) or `0` (black)
+> Enter padding as a fraction of the image size like `1/16` or `0` for none (`1/8` recommended)
+> Add more padding if a particularly wide or tall icon is not able to fit in the desired image size
+> Images will be saved in a newly made `images` folder
